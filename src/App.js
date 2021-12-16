125584bb15f83ed648275f1aa64d9953c9ac3a52
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-12-16 10:49:10 +0000

    add structure, i styled components, normalize css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-function App() {
-    return <div className="App"></div>;
-}
+import MainPage from "./views/MainPage";
+import GlobalStyle from "./components/styled/GlobalStyle";
+
+const App = () => {
+    return (
+        <>
+            <GlobalStyle />
+            <MainPage />
+        </>
+    );
+};
 
 export default App;
---
+++ src/views/MainPage.js
@@ -0,0 +1,5 @@
+const MainPage = () => {
+    return <div>appka</div>;
+};
+
+export default MainPage;
